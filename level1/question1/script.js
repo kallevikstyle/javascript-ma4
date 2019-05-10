14ee92df76e2897793eea72ea9247aeb67970290
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-05-10 10:14:50 +0200

    Removed argument e from eventlistener function

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,5 +1,5 @@
 const myButton = document.querySelector('#btn');
 
-myButton.addEventListener('click', function(e) {
+myButton.addEventListener('click', function() {
 	alert("Congratulations, you successfully clicked a button!");
 }, false);

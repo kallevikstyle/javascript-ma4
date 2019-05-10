caca19f0003267fcd5d1b626e26c92a159147d3c
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-05-11 00:41:23 +0200

    Completed level 1 question 2

---
+++ level1/question2/script.js
@@ -0,0 +1,63 @@
+const requestURL = 'https://jsonplaceholder.typicode.com/todos';
+const newRequest = new XMLHttpRequest();
+
+function addNewCards(data) {
+	let userId = 0;
+
+	// Iterate throug JSON content
+	for (let i = 0; i < data.length; i++) {
+		// Test if user ID is the same as the previous one
+		if (data[i].userId === userId) {
+			const sections = document.querySelectorAll('section');
+			const newCard = document.createElement('div');
+			const newCardTitle = document.createElement('h3');
+			const newCardId = document.createElement('p');
+			const newCardStatus = document.createElement('p');
+
+			// Get info about task
+			newCardTitle.textContent = data[i].title;
+			newCardId.textContent = 'ID: ' + data[i].id;
+			// Check if task is completed
+			if (data[i].completed) {
+				newCardStatus.textContent = 'Completed';
+				newCardStatus.setAttribute("class", "completed");
+			} else {
+				newCardStatus.textContent = 'Not completed';
+				newCardStatus.setAttribute("class", "not-completed");
+			}
+
+			// Create a new card
+			newCard.appendChild(newCardTitle);
+			newCard.appendChild(newCardId);
+			newCard.appendChild(newCardStatus);
+			// Append card to the last section
+			sections[sections.length - 1].appendChild(newCard);
+
+		// Create new section for new user ID
+		} else {
+			const todoContainer = document.querySelector('#todo-container');
+			const newSection = document.createElement('section');
+			const newCard = document.createElement('div');
+			const newH2 = document.createElement('h2');
+
+			// Create header for new user
+			newH2.textContent = 'USER ' + data[i].userId;
+			newCard.appendChild(newH2);
+			newSection.appendChild(newCard);
+			todoContainer.appendChild(newSection);
+
+			userId++;
+		}
+	}
+}
+
+// Send request for API
+newRequest.open('GET', requestURL);
+newRequest.responseType = 'json';
+newRequest.send();
+
+// Receive request
+newRequest.onload = function() {
+	const todoData = newRequest.response;
+	addNewCards(todoData);
+}
